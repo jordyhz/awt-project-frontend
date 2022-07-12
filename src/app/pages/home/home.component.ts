8cb63ddb11ded28c7342a4762322a0d052f206ce
jordyhz <zeufackjordy@gmail.com>
2022-07-12 04:56:02 +0200

    Update home.component.ts

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -11,28 +11,18 @@ export class HomeComponent implements OnInit {
   teams: any = [
     {
       name: 'Jordy Zeufack',
-      linkedin:
-        'https://www.linkedin.com/in/jordy-hendrix-zeufack-assongmo-47b0a11b5',
     },
     {
       name: 'Arsalan Harouni',
-      linkedin:
-        'https://www.linkedin.com/in/jordy-hendrix-zeufack-assongmo-47b0a11b5/',
     },
     {
       name: 'Nicodemus Aprianto',
-      linkedin:
-        'https://www.linkedin.com/in/jordy-hendrix-zeufack-assongmo-47b0a11b5/',
     },
     {
       name: 'Yuning Zhou',
-      linkedin:
-        'https://www.linkedin.com/in/jordy-hendrix-zeufack-assongmo-47b0a11b5/',
     },
     {
       name: 'Xueyao Ma',
-      linkedin:
-        'https://www.linkedin.com/in/jordy-hendrix-zeufack-assongmo-47b0a11b5/',
     },
   ];
 
