b2d316bdaf2d3580bad70930fc8c4ac96b143fe0
jordyhz <zeufackjordy@gmail.com>
2022-07-13 08:05:47 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/NaxyShare'));
+app.use(express.static(__dirname + '/dist/naxy-share'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/NaxyShare/index.html'));
+res.sendFile(path.join(__dirname+'/dist/naxy-share/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
